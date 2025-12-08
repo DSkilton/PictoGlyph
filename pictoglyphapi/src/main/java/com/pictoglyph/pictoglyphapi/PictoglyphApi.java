b7509e171cd575608775bcf193c8dfc5015f02a0
DSkilton <duncanskilton.ds@gmail.com>
2025-12-08 14:42:13 +0000

    Setup basic SpringBoot project - need to config MySQL

---
+++ pictoglyphapi/src/main/java/com/pictoglyph/pictoglyphapi/PictoglyphApi.java
@@ -0,0 +1,11 @@
+package com.pictoglyph.pictoglyphapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictoglyphApi {
+	public static void main(String[] args) {
+		SpringApplication.run(PictoglyphApi.class, args);
+	}
+}
---
+++ pictoglyphapi/src/test/java/com/pictoglyph/pictoglyphapi/PictoglyphApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pictoglyph.pictoglyphapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PictoglyphApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
