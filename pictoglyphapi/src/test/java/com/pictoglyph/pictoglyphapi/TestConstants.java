ff771957c3240b39665ce5f21f56e74928260733
DSkilton <duncanskilton.ds@gmail.com>
2026-08-21 12:38:03 +0100

    Refactored TestConstants

---
+++ pictoglyphapi/src/test/java/com/pictoglyph/pictoglyphapi/TestConstants.java
@@ -0,0 +1,10 @@
+package com.pictoglyph.pictoglyphapi;
+
+public class TestConstants {
+
+	private TestConstants() {	}
+
+	public static final String PICTOGLYPH_TEST_FILE_LOCATION = "..\\pictoglyph\\test_api\\A1.png";
+	public static final String SIGLIP_BASELINE_V1 = "SIGLIP_BASELINE_V1";
+	public static final String ABC_123_CHECKSUM = "abc123checksum";
+}
